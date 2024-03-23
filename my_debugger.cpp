6128690d66bf9a2496bc0ae7677c8d4ca98bc06e
msasongko17 <msasongko17@ku.edu.tr>
2024-03-24 00:30:20 +0300

    breakpoint

--- my_debugger.cpp
+++ my_debugger.cpp
@@ -5,7 +5,29 @@
 #include <sys/ptrace.h>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 #include "linenoise.h"
+
+class breakpoint {
+public:
+        breakpoint(pid_t pid, std::intptr_t addr)
+                : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{}
+        {}
+
+	breakpoint()
+                : m_pid{0}, m_addr{0}, m_enabled{false}, m_saved_data{}
+        {}
+        void enable();
+        void disable();
+
+        auto is_enabled() const -> bool { return m_enabled; }
+        auto get_address() const -> std::intptr_t { return m_addr; }
+private:
+        pid_t m_pid;
+        std::intptr_t m_addr;
+        bool m_enabled;
+        uint8_t m_saved_data; //data which used to be at the breakpoint address
+};
 
 class debugger {
 public:
@@ -13,13 +35,40 @@ public:
 		: m_prog_name{std::move(prog_name)}, m_pid{pid} {}
 
 	void run();
+	void set_breakpoint_at_address(std::intptr_t addr);
 
 private:
 	std::string m_prog_name;
 	pid_t m_pid;
+	std::unordered_map<std::intptr_t, breakpoint> m_breakpoints;
 	void continue_execution();
 	void handle_command(const std::string& line);
 };
+
+void debugger::set_breakpoint_at_address(std::intptr_t addr) {
+	std::cout << "Set breakpoint at address 0x" << std::hex << addr << std::endl;
+	breakpoint bp {m_pid, addr};
+	bp.enable();
+	m_breakpoints[addr] = bp;
+}
+
+void breakpoint::enable() {
+	auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+	m_saved_data = static_cast<uint8_t>(data & 0xff); //save bottom byte
+	uint64_t int3 = 0xcc;
+	uint64_t data_with_int3 = ((data & ~0xff) | int3); // set bottom byte to 0xcc
+	ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
+
+	m_enabled = true;
+}
+
+void breakpoint::disable() {
+	auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+	auto restored_data = ((data & ~0xff) | m_saved_data);
+	ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
+
+	m_enabled = false;
+}
 
 void debugger::run() {
 	int wait_status;
@@ -69,6 +118,10 @@ void debugger::handle_command(const std::string& line) {
 
 	if(is_prefix(command, "continue")) {
 		continue_execution();
+	}
+	else if(is_prefix(command, "break")) {
+		std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
+		set_breakpoint_at_address(std::stol(addr, 0, 16));
 	}
 	else {
 		std::cerr << "Unknown command\n";
