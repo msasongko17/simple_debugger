d0e04d57e098b3b330eccd2455e5215e1c59d4e7
msasongko17 <msasongko17@ku.edu.tr>
2024-03-24 22:59:56 +0300

    reading and writing to registers and memory locations

--- my_debugger.cpp
+++ my_debugger.cpp
@@ -7,7 +7,96 @@
 #include <sstream>
 #include <unordered_map>
 #include <sys/personality.h>
+#include <sys/user.h>
+#include <algorithm>
+#include <iomanip>
 #include "linenoise.h"
+
+enum class reg {
+	rax, rbx, rcx, rdx,
+	rdi, rsi, rbp, rsp,
+	r8, r9, r10, r11,
+	r12, r13, r14, r15,
+	rip, rflags, cs,
+	orig_rax, fs_base,
+	gs_base,
+	fs, gs, ss, ds, es
+};
+
+constexpr std::size_t n_registers = 27;
+
+struct reg_descriptor {
+	reg r;
+	int dwarf_r;
+	std::string name;
+};
+
+const std::array<reg_descriptor, n_registers> g_register_descriptors {{
+	{ reg::r15, 15, "r15"},
+	{ reg::r14, 14, "r14"},
+	{ reg::r13, 13, "r13"},
+	{ reg::r12, 12, "r12"},
+	{ reg::rbp, 6, "rbp" },
+	{ reg::rbx, 3, "rbx" },
+	{ reg::r11, 11, "r11" },
+	{ reg::r10, 10, "r10" },
+	{ reg::r9, 9, "r9" },
+	{ reg::r8, 8, "r8" },
+	{ reg::rax, 0, "rax" },
+	{ reg::rcx, 2, "rcx" },
+	{ reg::rdx, 1, "rdx" },
+	{ reg::rsi, 4, "rsi" },
+	{ reg::rdi, 5, "rdi" },
+	{ reg::orig_rax, -1, "orig_rax" },
+	{ reg::rip, -1, "rip" },
+	{ reg::cs, 51, "cs" },
+	{ reg::rflags, 49, "eflags" },
+	{ reg::rsp, 7, "rsp" },
+	{ reg::ss, 52, "ss" },
+	{ reg::fs_base, 58, "fs_base" },
+	{ reg::gs_base, 59, "gs_base" },
+	{ reg::ds, 53, "ds" },
+	{ reg::es, 50, "es" },
+	{ reg::fs, 54, "fs" },
+	{ reg::gs, 55, "gs" },
+}};
+
+uint64_t get_register_value(pid_t pid, reg r) {
+	user_regs_struct regs;
+	ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
+
+	auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), [r](auto&& rd) {return rd.r == r; });
+
+	return *(reinterpret_cast<uint64_t*>(&regs) + (it - begin(g_register_descriptors)));
+}
+
+void set_register_value(pid_t pid, reg r, uint64_t value) {
+	user_regs_struct regs;
+	ptrace(PTRACE_GETREGS, pid, nullptr, &regs);
+	auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), [r](auto&& rd) {return rd.r == r;});
+
+	*(reinterpret_cast<uint64_t*>(&regs) + (it - begin(g_register_descriptors))) = value;
+	ptrace(PTRACE_SETREGS, pid, nullptr, &regs);
+}
+
+uint64_t get_register_value_from_dwarf_register (pid_t pid, unsigned regnum) {
+	auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), [regnum](auto&& rd) {return rd.dwarf_r == regnum; });
+	if (it == end(g_register_descriptors)) {
+		throw std::out_of_range("Unknown dwarf register");
+	}
+
+	return get_register_value(pid, it->r);
+}
+
+std::string get_register_name(reg r) {
+	auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), [r](auto&& rd) { return rd.r == r; });
+	return it->name;
+}
+
+reg get_register_from_name(const std::string& name) {
+	auto it = std::find_if(begin(g_register_descriptors), end(g_register_descriptors), [name](auto&& rd) { return rd.name == name; });
+	return it->r;
+}
 
 class breakpoint {
 public:
@@ -44,7 +133,17 @@ private:
 	std::unordered_map<std::intptr_t, breakpoint> m_breakpoints;
 	void continue_execution();
 	void handle_command(const std::string& line);
+	void dump_registers();
+	uint64_t read_memory(uint64_t address);
+	void write_memory(uint64_t address, uint64_t value);
 };
+
+void debugger::dump_registers() {
+        for (const auto& rd : g_register_descriptors) {
+                std::cout << rd.name << " 0x"
+                        << std::setfill('0') << std::setw(16) << std::hex << get_register_value(m_pid, rd.r) << std::endl;
+        }
+}
 
 void debugger::set_breakpoint_at_address(std::intptr_t addr) {
 	std::cout << "Set breakpoint at address 0x" << std::hex << addr << std::endl;
@@ -96,6 +195,14 @@ void debugger::continue_execution() {
 	std::cerr << "after waitpid\n";
 }
 
+uint64_t debugger::read_memory(uint64_t address) {
+	return ptrace(PTRACE_PEEKDATA, m_pid, address, nullptr);
+}
+
+void debugger::write_memory(uint64_t address, uint64_t value) {
+	ptrace(PTRACE_POKEDATA, m_pid, address, value);
+}
+
 std::vector<std::string> split(const std::string &s, char delimiter) {
         std::vector<std::string> out{};
         std::stringstream ss {s};
@@ -123,6 +230,29 @@ void debugger::handle_command(const std::string& line) {
 	else if(is_prefix(command, "break")) {
 		std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
 		set_breakpoint_at_address(std::stol(addr, 0, 16));
+	}
+	else if (is_prefix(command, "register")) {
+		if (is_prefix(args[1], "dump")) {
+			dump_registers();
+		}
+		else if (is_prefix(args[1], "read")) {
+			std::cout << get_register_value(m_pid, get_register_from_name(args[2])) << std::endl;
+		}
+		else if (is_prefix(args[1], "write")) {
+			std::string val {args[3], 2};
+			set_register_value(m_pid, get_register_from_name(args[2]), std::stol(val, 0, 16));
+		}
+	}
+	else if(is_prefix(command, "memory")) {
+		std::string addr {args[2], 2};
+
+		if(is_prefix(args[1], "read")) {
+			std::cout << std::hex << read_memory(std::stol(addr, 0, 16)) << std::endl;
+		}
+		if (is_prefix(args[1], "write")) {
+			std::string val{ args[3], 2};
+			write_memory(std::stol(addr, 0, 16), std::stol(val, 0, 16));
+		}
 	}
 	else {
 		std::cerr << "Unknown command\n";
